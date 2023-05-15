bfcf5158f13ed3a1d9bcca8aedf93121218128ee
Sayed Riaydh <$email>
2023-05-16 01:51:31 +0600

    feat: publish 1.0.15

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
+import replace from "@rollup/plugin-replace";
 
 import packageJson from "./package.json" assert { type: "json" };
 
@@ -24,7 +25,16 @@ export default [
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
+      replace({
+        "process.env.NODE_ENV": JSON.stringify("production"),
+        "process.env.REACT_APP_SC_ATTR": JSON.stringify("production"),
+        "process.env.SC_ATTR": JSON.stringify("production"),
+        "process.env.SC_DISABLE_SPEEDY": JSON.stringify("production"),
+        "process.env.REACT_APP_SC_DISABLE_SPEEDY": JSON.stringify("production"),
+        "process.env": JSON.stringify("production"),
+      }),
     ],
+    external: ["styled-components"],
   },
   {
     input: "dist/esm/types/index.d.ts",
